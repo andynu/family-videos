af723cea5b5d46be53ed71d55d901660b93c569a
andy <andynu@gmail.com>
2014-12-28 12:49:17 -0500

    add video uploading to form

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -39,7 +39,10 @@ class VideoUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(avi mkv mov)
+    %w(
+      avi mkv mov mp4
+      gif png jpg jpeg
+    )
   end
 
   # Override the filename of the uploaded files:
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141228172707) do
+ActiveRecord::Schema.define(version: 20141228174358) do
 
   create_table "videos", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "video",      limit: 255
   end
 
 end
